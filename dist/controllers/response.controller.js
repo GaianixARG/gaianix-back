4d8f4ff66d19a4cd0df2f7bb32d5a43ae7b14943
Francisco <fnasif@fi.uba.ar>
2025-10-29 11:35:42 +0100

    intento deploy 2

--- dist/controllers/response.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendData = void 0;
-const sendData = (response, status, data) => {
-    return response.status(status).send(data);
-};
-exports.sendData = sendData;
-exports.default = exports.sendData;
